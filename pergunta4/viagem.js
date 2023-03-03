217c3824de0086e819fff689672a167f0e294643
Jhone Sthan <60678458+jhonesthan@users.noreply.github.com>
2023-03-03 05:19:19 -0400

    Add files via upload

---
+++ pergunta4/viagem.js
@@ -0,0 +1,17 @@
+//  Considerando que a distância entre Ribeirão Preto e Franca é de 100 km, 
+//  e que o carro e o caminhão estão se movendo em direções opostas, eles se encontrarão 
+//  no meio do caminho, ou seja, a 50 km de cada cidade. Como o carro está mais próximo e
+//  há dois pedágios no caminho e que o caminhão leva 5 minutos extras para passar em cada
+//  um deles, precisamos levar em conta o tempo extra que o caminhão leva em relação ao carro. 
+//  Sabendo que a distância entre Ribeirão Preto e Franca é de 100 km, e que a velocidade do 
+//  carro é de 110 km/h e do caminhão é de 80 km/h, podemos calcular o tempo que cada um leva 
+//  para percorrer a distância:
+ 
+//  Tempo do carro = 100 km ÷ 110 km/h = 0,91 hora = 54,5 minutos
+ 
+//  Tempo do caminhão = 100 km ÷ 80 km/h = 1,25 hora = 75 minutos
+ 
+//  O caminhão leva, portanto, um tempo extra de 2 x 5 = 10 minutos nos pedágios. Isso significa
+//  que, quando eles se encontrarem, o caminhão estará a uma distância um pouco maior da França do 
+//  que o carro estará de Ribeirão Preto. No entanto, a diferença de distância será muito pequena
+//  aproximadamente 30 minutos
